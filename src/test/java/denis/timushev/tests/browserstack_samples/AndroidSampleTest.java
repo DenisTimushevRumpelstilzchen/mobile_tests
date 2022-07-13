4a29f15aeda82f3c5eab226abd9001c1ce428643
Your NameDenis <tdv14011984@gmail.com>
2022-07-13 20:35:04 +0300

    aAndroidSampleTest

--- src/test/java/denis/timushev/tests/browserstack_samples/AndroidSampleTest.java
+++ src/test/java/denis/timushev/tests/browserstack_samples/AndroidSampleTest.java
@@ -1,4 +1,4 @@
-/* package denis.timushev.tests.browserstack_samples;
+package denis.timushev.tests.browserstack_samples;
 
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
@@ -65,4 +65,3 @@ public class AndroidSampleTest {
 
     }
 }
-*/
