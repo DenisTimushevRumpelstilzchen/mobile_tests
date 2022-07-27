1e373b5a3e1495fd0a3f27275c68459593fb71d9
Your NameDenis <tdv14011984@gmail.com>
2022-07-27 20:03:09 +0300

    add TestBase

--- src/test/java/denis/timushev/tests/TestBase.java
+++ src/test/java/denis/timushev/tests/TestBase.java
@@ -1,22 +1,24 @@
 package denis.timushev.tests;
 
+import autotests.config.BrowserstackMobileDriver;
+import autotests.helpers.Attach;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import drivers.BrowserstackMobileDriver;
-import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
+import static autotests.helpers.Attach.sessionId;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
-import static helpers.Attach.sessionId;
 import static io.qameta.allure.Allure.step;
 
+
 public class TestBase {
+
     @BeforeAll
-    public static void setup() {
+    public static void setUp() {
         Configuration.browser = BrowserstackMobileDriver.class.getName();
         Configuration.browserSize = null;
     }
@@ -24,7 +26,6 @@ public class TestBase {
     @BeforeEach
     public void startDriver() {
         addListener("AllureSelenide", new AllureSelenide());
-
         open();
     }
 
